0fc3b2e75143e8721d00641c9c667f96b4771a0e
guglielmoc <guglielmoc@google.com>
2025-11-17 09:20:15 +0000

    remove formatting configuration from eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,14 +3,12 @@
 import eslint from '@eslint/js';
 import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
-import eslintConfigPrettier from 'eslint-config-prettier';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import eslintConfigPrettier from 'eslint-config-prettier'; 
 
 export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   eslintConfigPrettier,
-  eslintPluginPrettierRecommended,
   {
     ignores: ['dist/', 'node_modules/']
   },
