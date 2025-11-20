d948917cd34f6995a5349c97d03e9013d19bcd33
guglielmoc <guglielmoc@google.com>
2025-11-20 11:07:06 +0000

    first commint implementation of authentication on server side

--- src/server/express/json_rpc_handler.ts
+++ src/server/express/json_rpc_handler.ts
@@ -37,7 +37,12 @@ export function jsonRpcHandler(options: JsonRpcHandlerOptions): RequestHandler {
 
   router.post('/', async (req: Request, res: Response) => {
     try {
+<<<<<<< HEAD
       const user = await options.userBuilder(req);
+=======
+      const agentCard = await options.requestHandler.getAgentCard();
+      checkAuthentication(req.headers, agentCard);
+>>>>>>> cd9163c (first commint implementation of authentication on server side)
       const context = new ServerCallContext(
         getRequestedExtensions(req.header(HTTP_EXTENSION_HEADER)),
         user ?? new UnauthenticatedUser()
