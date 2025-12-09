40c126438717763cfdcc130795a76d990ca981cc
guglielmoc <guglielmoc@google.com>
2025-12-09 16:13:21 +0000

    add type in other locations

--- src/client/transports/json_rpc_transport.ts
+++ src/client/transports/json_rpc_transport.ts
@@ -30,7 +30,7 @@ import {
   GetTaskPushNotificationConfigParams,
   GetAuthenticatedExtendedCardSuccessResponse,
 } from '../../types.js';
-import { A2AStreamEventData, SendMessageResult } from '../client.js';
+import { A2AStreamEventData, A2AStreamEventDataResubscribe, SendMessageResult } from '../client.js';
 import { RequestOptions } from '../multitransport-client.js';
 import { Transport, TransportFactory } from './transport.js';
 
@@ -156,7 +156,7 @@ export class JsonRpcTransport implements Transport {
   async *resubscribeTask(
     params: TaskIdParams,
     options?: RequestOptions
-  ): AsyncGenerator<A2AStreamEventData, void, undefined> {
+  ): AsyncGenerator<A2AStreamEventDataResubscribe, void, undefined> {
     yield* this._sendStreamingRequest('tasks/resubscribe', params, options);
   }
 
